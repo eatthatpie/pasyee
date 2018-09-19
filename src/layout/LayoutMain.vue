c61d59dc9211391f3fc8c0c05cfb31a5c056be15
eatthatpie <tomek.pugn@gmail.com>
2018-09-19 19:31:48 +0200

    add basic layout views

---
+++ src/layout/LayoutMain.vue
@@ -0,0 +1,17 @@
+<template>
+    <main class="layout-main">
+        <div class="wrapper">
+            
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
