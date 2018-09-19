7c7b48c59a6f3e74187aaa05fa742af87fba2409
eatthatpie <tomek.pugn@gmail.com>
2018-09-19 19:32:08 +0200

    add basic nav components

---
+++ src/components/Hamburger.vue
@@ -0,0 +1,73 @@
+<template>
+    <button class="hamburger" :class="{ 'is-active': isActive }">
+        <span v-for="(n, i) in 3" :key="i" />
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        isActive: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+@import './../assets/scss/mixins/_mixins.scss';
+@import './../assets/scss/variables/_variables.scss';
+
+.hamburger {
+    @include dimensions(40px, 32px);
+    position: absolute;
+    background-color: transparent;
+    outline: 0;
+    border: 0;
+    right: $value-container-padding - 5px;
+    top: 15px;
+
+    span {
+        @include transition();
+        @include dimensions(30px, 2px);
+        background-color: $color-default;
+        position: absolute;
+        left: 5px;
+        
+        &:nth-child(1) {
+            top: 5px;
+        }
+
+        &:nth-child(2) {
+            top: 15px;
+        }
+
+        &:nth-child(3) {
+            top: 25px;
+        }
+    }
+
+    &.is-active {
+        span {
+            &:nth-child(1) {
+                @include transform(translateY(10px) rotate(-45deg));
+            }
+
+            &:nth-child(2) {
+                opacity: 0;
+            }
+
+            &:nth-child(3) {
+                @include transform(translateY(-10px) rotate(45deg));
+            }
+        }
+    }
+
+    .is-nav-open & {
+        span {
+            background-color: $color-primary;
+        }
+    }
+}
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="nav">
+        <ul>
+            <li>
+                <a href="/">Generate Password</a>
+            </li>
+            <li>
+                <a href="/htpasswd">Generate Password For .htpasswd</a>
+            </li>
+            <li>
+                <a href="/how-to-memorize-your-password">How To Memorize Your Passwords</a>
+            </li>
+            <li>
+                <a href="https://github.com/eatthatpie/pasyee" target="_blank">Source on GitHub</a>
+            </li>
+            <li>
+                <a href="/send-feedback">Send Feedback</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import './../assets/scss/mixins/_mixins.scss';
+@import './../assets/scss/variables/_variables.scss';
+
+.nav {
+    @include position-fixed-cover();
+    visibility: hidden;
+    opacity: 0;
+    top: $value-header-height;
+
+    a {
+        color: $color-primary;
+        font-weight: $font-weight-extrabold;
+        font-size: 18px;
+    }
+
+    li {
+        @include transition(.6s);
+        @include transition-delay(0s);
+        @include transform(translateY(10px));
+        -webkit-backface-visibility: hidden;
+        backface-visibility: hidden;
+        padding: 0 $value-container-padding;
+        opacity: 0;
+
+        &:nth-child(n + 2) {
+            margin-top: 15px;
+        }
+
+        > a {
+            display: block;
+            text-align: center;
+        }
+    }
+
+    ul {
+        @include position-center-vertically(-62px);
+        width: 100%;
+    }
+
+    .is-nav-open & {
+        visibility: visible;
+        opacity: 1;
+
+        li {
+            @include transform-reset();
+            opacity: 1;
+
+            @for $i from 1 through 10 {
+                &:nth-child(#{$i}) {
+                    $d: ($i / 8);
+                    @include transition-delay(#{$d}s);
+                }
+            }
+        }
+    }
+}
+</style>
