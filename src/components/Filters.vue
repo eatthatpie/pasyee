051b22654c51aa27e13ea378c0a943f68b4c7040
eatthatpie <tomek.pugn@gmail.com>
2018-10-03 20:47:10 +0200

    update filters component

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,19 +1,41 @@
 <template>
-    <div :class="[ 'filters', { 'is-open': isOpen } ]" @click="isOpen = !isOpen">
-        <a class="filters-label">characters... &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;   easy to...</a>
-        <div class="filters-placeholder">
-            <div class="filters-leaf">
-                <div class="filters-item">
-                    letters <i class="icon-checkmark" />
+    <div :class="[ 'filters', { 'is-open': isOpen } ]">
+        <div class="filters-labels">
+            <a class="filters-label" @click="isCharactersOpen = true">
+                <span>characters...</span>
+            </a>
+            <a class="filters-label" @click="isEasyToOpen = true">
+                <span>easy to...</span>
+            </a>
+        </div>
+        <div class="filters-placeholder" v-click-outside="onClickOutsidePlaceholder">
+            <div :class="[ 'filters-leaf', { 'is-open': isCharactersOpen } ]">
+                <div class="filters-list">
+                    <div class="filters-item is-active">
+                        letters <i class="icon-checkmark" />
+                    </div>
+                    <div class="filters-item is-active">
+                        numbers <i class="icon-checkmark" />
+                    </div>
+                    <div class="filters-item">
+                        light specials <i class="icon-checkmark" />
+                    </div>
+                    <div class="filters-item">
+                        heavy specials <i class="icon-checkmark" />
+                    </div>
                 </div>
-                <div class="filters-item">
-                    numbers <i class="icon-checkmark" />
-                </div>
-                <div class="filters-item" style="opacity: .3">
-                    light specials <i class="icon-checkmark" />
-                </div>
-                <div class="filters-item" style="opacity: .3">
-                    heavy specials <i class="icon-checkmark" />
+            </div>
+            <div class="filters-leaf" :class="{ 'is-open': isEasyToOpen }">
+                <div class="filters-list">
+                    <div class="filters-item is-active">
+                        write <i class="icon-checkmark" />
+                    </div>
+                    <div class="filters-item">
+                        read <i class="icon-checkmark" />
+                    </div>
+                    <div class="filters-item">
+                        remember <i class="icon-checkmark" />
+                    </div>
                 </div>
             </div>
         </div>
@@ -21,15 +43,44 @@
 </template>
 
 <script>
+import ClickOutside from 'vue-click-outside'
+
 export default {
+    directives: {
+        ClickOutside
+    },
     data () {
         return {
-            isOpen: false
+            isCharactersOpen: false,
+            isEasyToOpen: false
+        }
+    },
+    computed: {
+        isOpen () {
+            return this.isCharactersOpen || this.isEasyToOpen
         }
     },
     methods: {
         close () {
-            this.isOpen = false
+            this.isCharactersOpen = false
+            this.isEasyToOpen = false
+        },
+        onClickOutsidePlaceholder (e) {
+            if (e.target.className != 'filters-label' && e.target.parentNode.className != 'filters-label') {
+                this.close()
+            }
+        }
+    },
+    watch: {
+        isCharactersOpen () {
+            if (this.isCharactersOpen) {
+                this.isEasyToOpen = false
+            }
+        }, 
+        isEasyToOpen () {
+            if (this.isEasyToOpen) {
+                this.isCharactersOpen = false
+            }
         }
     }
 }
@@ -41,7 +92,6 @@ export default {
 
 .filters {
     @include transition(.3s);
-    background-color: $color-primary-light;
     position: relative;
     height: 33px;
 
@@ -71,17 +121,36 @@ export default {
         top: 100%;
         height: 100%;
         padding: 20px;
+        opacity: 0;
         z-index: 2;
     }
 
+    &-labels {
+        display: flex;
+        justify-content: space-between;
+    }
+
     &-label {
-        @include transition();
-        @include transition-delay(.2s);
         color: $color-default;
+        background-color: $color-primary-light;
         text-align: center;
         display: block;
+        width: calc(50% - 5px);
         padding: 5px 0 0;
         height: 33px;
+
+        span {
+            @include transition();
+            @include transition-delay(.1s);
+            opacity: 1;
+        }
+    }
+
+    &-list {
+        @include position-center-vertically(-19px);
+        position: absolute;
+        left: 0;
+        right: 0;
     }
 
     &-item {
@@ -90,11 +159,13 @@ export default {
         font-weight: $font-weight-bold;
         padding: 3px 0;
         opacity: 0;
+
+        [class^="icon-"] {
+            margin-left: 5px;
+        }
     }
 
     &.is-open {
-        //background-color: $color-default;
-
         &:before {
             @include transition(.3s);
             visibility: visible;
@@ -104,17 +175,25 @@ export default {
 
     &.is-open & {
         &-leaf {
-            @include transition(.3s);
-            @include transform(translateY(calc(33px - 100%)));
-            //background-color: $color-default;
+            &.is-open {
+                @include transition(.3s);
+                @include transform(translateY(calc(33px - 100%)));
+                opacity: 1;
+            }
         }
 
         &-label {
-            opacity: 0;
+            span {
+                opacity: 0;
+            }
         }
 
         &-item {
-            opacity: 1;
+            opacity: .3;
+
+            &.is-active {
+                opacity: 1;
+            }
         }
     }
 }
