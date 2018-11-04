5fe0b1c9c446b359eff98e9234a4f9564a69a597
eatthatpie <tomek.pugn@gmail.com>
2018-11-04 14:49:30 +0100

    update password strength plugin

--- src/plugins/password-strength/PasswordStrength.js
+++ src/plugins/password-strength/PasswordStrength.js
@@ -1,3 +1,6 @@
+import { PasswordRegexPenaltyFactors } from './PasswordRegexPenaltyFactors'
+import { PasswordRegexScoreFactors } from './PasswordRegexScoreFactors'
+
 export const PasswordStrength = {
     data: {
         password: ''
@@ -5,11 +8,42 @@ export const PasswordStrength = {
     setPassword (password) {
         this.password = password
     },
-    getStrength: (password) => {
+    getStrength (password) {
         if (password) {
-            this.password = password
+            this.setPassword(password)
+        } 
+
+        let strength = 0
+
+        let regexFactorsArray = [].concat(
+            Object.values(PasswordRegexScoreFactors), 
+            Object.values(PasswordRegexPenaltyFactors)
+        )
+
+        for (let i in regexFactorsArray) {
+            let prf = regexFactorsArray[i],
+                characterMatches = this.password.match(prf.regex)
+
+            if (characterMatches === null) {
+                continue;
+            }
+    
+            let length = characterMatches.length
+
+            if (prf.maxDistinctCharacterOccurencies) {
+                let maxLength = this.getDistinctCharacters(characterMatches).length * prf.maxDistinctCharacterOccurencies
+
+                length = characterMatches.length > maxLength ? maxLength : characterMatches.length
+            }
+
+            strength += length * prf.factor
         }
 
-        return Math.round(Math.random() * 100)
+        return strength > 100 ? 100 : strength
+    },
+    getDistinctCharacters (characters) {
+        return characters.filter((v, i, self) => {
+            return self.indexOf(v) === i
+        })
     }
 }
