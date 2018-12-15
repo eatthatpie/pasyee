51c6c28ba608caa7cf6bef6e520a93200875994f
eatthatpie <tomek.pugn@gmail.com>
2018-12-15 13:00:55 +0100

    update pages

---
+++ src/pages/PageRoadmap.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="__page">
+        <div class="container container-slim space">
+            <contents>
+                <h1 class="contents-center contents-header contents-lead">Roadmap</h1>
+                <p>
+                    Current roadmap for <strong>version 1.*</strong> (2019):
+                </p>
+                <ul> 
+                    <li>Implement `easy to say`, `easy to read` and `easy to remember` randomization algorithms;</li> 
+                </ul>
+                <p>
+                    Roadmap for <strong>Pasyee 2.0</strong> (2019/2020):
+                </p>
+                <ul>
+                    <li>Create safe and local passwords storage;</li>
+                </ul>
+            </contents>
+        </div>
+    </div>
+</template>
+
+<script>
+import Contents from './../components/Contents'
+
+export default {
+    components: {
+        Contents
+    }
+}
+</script>
